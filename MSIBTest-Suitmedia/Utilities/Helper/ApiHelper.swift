405dd2a260d13ef9c1c8acbc6f9452358a3b53bc
Rifqi Triginandri <rifqit19@gmail.com>
2023-12-25 04:03:58 +0700

    Create ApiHelper.swift

---
+++ MSIBTest-Suitmedia/Utilities/Helper/ApiHelper.swift
@@ -0,0 +1,42 @@
+//
+//  ApiHelper.swift
+//  MSIBTest-Suitmedia
+//
+//  Created by rifqi triginandri on 24/12/23.
+//
+
+import Foundation
+
+
+class ApiHelper{
+    
+    static let shared = ApiHelper()
+    
+    private let apiClient = ApiClient.shared
+    
+    private init() {}
+    
+    func fetchUsers(page: Int, perPage: Int, completion: @escaping ([User]?, Error?) -> Void){
+        let parameters = Constants.Parameters.Users.getUsers(page: page, perPage: perPage)
+        
+        apiClient.fetchData(endpoint: Constants.Endpoints.users, parameters: parameters) { data, error in
+
+            guard let data = data, error == nil else{
+                completion(nil, error)
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(UserResponse.self, from: data)
+
+                completion(result.data, nil)
+            }catch{
+                completion(nil, error)
+            }
+        }
+    }
+}
+
+
+
