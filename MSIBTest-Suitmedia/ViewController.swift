2ae6e4a0c21ed7f07c4408edb3b5f9093a569f95
Rifqi Triginandri <rifqit19@gmail.com>
2023-12-24 19:29:59 +0700

    Initial Commit

---
+++ MSIBTest-Suitmedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSIBTest-Suitmedia
+//
+//  Created by rifqi triginandri on 24/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
